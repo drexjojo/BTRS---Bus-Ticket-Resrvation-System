ac01f7ac6d23697a5afe86a38120a85054d6cfb2
Aditya Motwani <rajmanvarchamp@gmail.com>
2018-04-08 14:42:11 +0530

    Get paths

--- Bus_reservation/src/bus/views.py
+++ Bus_reservation/src/bus/views.py
@@ -23,6 +23,27 @@ def search_route(route_dict,start,end,path=[]) :
     return paths
 
 # def get_all_routes(area_from_id,area_to_id):
+def func(path):
+    last_departure = -1
+    bus_routes = {}
+    count = 1
+    for stop_no in range(len(path)-1):
+        start_bus_nos = Stop.objects.filter(area_name = path[stop_no])
+        end_bus_nos = Stop.objects.filter(area_name = path[stop_no+1])
+        bus_routes[count] = []
+        for i in start_bus_nos:
+            for j in end_bus_nos:
+                bus_info_list = Bus.objects.filter(arriving_from_id=i,depature_at_id=j)
+                for k in bus_info_list:
+                    # if k.arriving_from_id > last_departure:
+                    print str(k.slug)
+                    bus_routes[count].append(k.slug)
+                    last_departure = k.depature_at_id
+                    print "GG"
+        count += 1
+    print bus_routes
+
+
 def search_bus(request,template_name ='bus/search_bus.html'):
     all_routes = {}
     page_title = 'Book a ticket'
@@ -38,7 +59,7 @@ def search_bus(request,template_name ='bus/search_bus.html'):
         route_dict = {}
         bus_info_list = Bus.objects.filter(arriving_from_id=area_from_id,depature_at_id=area_to_id)
         all_busses = Bus.objects.all()
-        for bus in all_busses :  
+        for bus in all_busses :
             arriving_from = Stop.objects.filter(id = int(bus.arriving_from_id))
             departing_at = Stop.objects.filter(id = int(bus.depature_at_id))
             if arriving_from[0].area_name in route_dict :
@@ -48,7 +69,9 @@ def search_bus(request,template_name ='bus/search_bus.html'):
 
         paths = search_route(route_dict,start_area_name,stop_area_name)
         unique_paths = [list(x) for x in set(tuple(x) for x in paths)]
-        print unique_paths
+        # for path in unique_paths:
+        func(unique_paths[0])
+        # print unique_paths
 
 
     return render(request,template_name, locals())
